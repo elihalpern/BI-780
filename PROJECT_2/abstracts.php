eee45bd5eeadcf0e0506d8656ecea9f8280eac73
Eli <eli@Elis-iMac.local>
2017-09-23 13:46:20 -0400

    Core functionality in place. Retrieves all abstracts for given query.

--- PROJECT_2/abstracts.php
+++ PROJECT_2/abstracts.php
@@ -1,9 +1,23 @@
 <html>
     <body>
         <?php
+            define('SIZE', 1000);
             $searchTerms = $_GET['searchTerms'];
-            $url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json&retmax=1000&term='.urlencode($searchTerms);
-            echo $url;
+            $url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json&retmax='.SIZE.'&term='.urlencode($searchTerms);
+            $json = file_get_contents($url);
+            $data = json_decode($json);
+            $count = $data->esearchresult->count;
+            for($i = 0; $i < $count; $i+=SIZE)
+            {
+                $url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json&retmax='.SIZE.'&retstart='.$i.'&term='.urlencode($searchTerms);
+                $json = file_get_contents($url);
+                $data = json_decode($json);
+                foreach($data->esearchresult->idlist as $id)
+                {
+                    $abstract = file_get_contents('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=text&rettype=abstract&id='.$id);
+                    echo '<p>'.$abstract.'</p>';
+                }
+            }
         ?>
     </body>
 </html>
